119fa604eec3327003221f3c03fec7103442841e
trSno1 <aguilarjayden93@gmail.com>
2025-11-14 20:36:50 +0000

    edits

--- week 13  csp/3 — Lists and List Functions.py
+++ week 13  csp/3 — Lists and List Functions.py
@@ -5,32 +5,63 @@
 # Creating lists, indexing, slicing, appending, popping, sorting, reversing.
 
 # Examples:
+# collections are used to store multiple items in a single variable
+#lists are oredred collecrtions of items
+#lists are mutable, meaning you can change their content
+#lists are created using square bgrackets []
 
-my_list = ['apple', 'banana', 'cherry']
-print(my_list[0])         # apple
-print(my_list[1:])        # ['banana', 'cherry']
+my_list = [1,2,3,4,5]
+print(my_list) # [1,2,3,4,5]
+print(type(my_list)) # <class 'list'
 
-my_list.append('grape')
+print(my_list[0])
+print(my_list[1:4])
+print(my_list[0:])
+#modifying lists
+#adding an item to the end of the list
+my_list.append(6)
+my_list.append(7)
+my_list.append(8)
 print(my_list)
 
-my_list.pop(1)
+my_list.extend([10,11,12,13,14])
 print(my_list)
 
-numbers = [3, 1, 4, 2]
-numbers.sort()
-print(numbers)
+my_list.extend(list(range(15,515)))
+print(my_list)
+
+my_list.extend(list(range(15,1115)))
+print(my_list)
+
+# instead of creating seperate variables for each item, we can store them in a list
+#this makes our job easier, when we need to manage multiple variables
+#performance task answer
+
+# my_list = ['apple', 'banana', 'cherry']
+# print(my_list[0])         # apple
+# print(my_list[1:])        # ['banana', 'cherry']
+
+# my_list.append('grape')
+# print(my_list)
+
+# my_list.pop(1)
+# print(my_list)
+
+# numbers = [3, 1, 4, 2]
+# numbers.sort()
+# print(numbers)
 
 
-# Practice Problems:
+# # Practice Problems:
 
-# Create a list with 5 of your favorite foods.
+# # Create a list with 5 of your favorite foods.
 
-# Print the second and last item.
+# # Print the second and last item.
 
-# Add a new item using .append().
+# # Add a new item using .append().
 
-# Remove the first item using .pop(0).
+# # Remove the first item using .pop(0).
 
-# Reverse your list using .reverse().
+# # Reverse your list using .reverse().
 
-# Create a list of 3 lists (matrix), and access the middle element.
+# # Create a list of 3 lists (matrix), and access the middle element.
